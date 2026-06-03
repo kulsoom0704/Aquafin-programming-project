6fa83a50a5384574c2bfdd90e6bf1ac42ec75260
Mohamed Zouljami <mohamed.zljm@gmail.com>
2026-06-03 19:47:25 +0200

    voorraad verhogen als artikel al bestaat

--- app/Http/Controllers/MateriaalController.php
+++ app/Http/Controllers/MateriaalController.php
@@ -20,15 +20,25 @@ class MateriaalController extends Controller
         return view('materiaal.create');
     }
 
-    // Sla het nieuwe artikel op in de database
+    // Sla het nieuwe artikel op of verhoog de voorraad
     public function store(Request $request)
     {
-        Materiaal::create([
-            'artikelnummer' => $request->artikelnummer,
-            'omschrijving'  => $request->omschrijving,
-            'locatie'       => $request->locatie,
-            'beschikbaar'   => $request->beschikbaar,
-        ]);
+        // Kijk of het artikel al bestaat
+        $materiaal = Materiaal::where('artikelnummer', $request->artikelnummer)->first();
+
+        if ($materiaal) {
+            // Artikel bestaat al, verhoog de voorraad
+            $materiaal->beschikbaar += $request->beschikbaar;
+            $materiaal->save();
+        } else {
+            // Artikel bestaat niet, maak een nieuw aan
+            Materiaal::create([
+                'artikelnummer' => $request->artikelnummer,
+                'omschrijving'  => $request->omschrijving,
+                'locatie'       => $request->locatie,
+                'beschikbaar'   => $request->beschikbaar,
+            ]);
+        }
 
         return redirect('/materiaal');
     }
