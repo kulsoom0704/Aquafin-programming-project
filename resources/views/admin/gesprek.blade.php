b789715f288be48e6dccbe82a5f0addaef3e67e9
sabrinemouchane <sabrine.mouchane@student.ehb.be>
2026-06-18 14:29:54 +0200

    gesprek pagina documentatie toegevoegd

--- resources/views/admin/gesprek.blade.php
+++ resources/views/admin/gesprek.blade.php
@@ -92,6 +92,7 @@
 
                     @else
 
+                        <!-- Technieker bericht -->
                         <div class="flex justify-start mb-4">
 
                             <div class="bg-white px-5 py-3 rounded-3xl rounded-bl-md max-w-sm shadow">
