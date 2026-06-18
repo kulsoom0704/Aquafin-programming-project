68990b4e4543c35a4076cd7fdc1f62d908684ceb
kulsoom0704 <kulsoomhifazat2@gmail.com>
2026-06-18 12:12:11 +0200

    wijzigingsverzoeken en meldingen gedocumenteerd

--- app/Http/Controllers/WijzigingsverzoekController.php
+++ app/Http/Controllers/WijzigingsverzoekController.php
@@ -7,11 +7,15 @@ use App\Models\Materiaal;
 use App\Models\Melding;
 use Illuminate\Http\Request;
 
+// WijzigingsverzoekController beheert aanvragen van magazijniers
+// om bestaande materialen te laten aanpassen door een administrator.
+
 class WijzigingsverzoekController extends Controller
 {
     // Toon het wijzigingsformulier
     public function create($id)
     {
+        // Haalt het geselecteerde materiaal op.
         $materiaal = Materiaal::find($id);
         return view('materiaal.wijzigen', compact('materiaal'));
     }
@@ -19,6 +23,7 @@ class WijzigingsverzoekController extends Controller
     // Sla het wijzigingsverzoek op
     public function store(Request $request, $id)
     {
+        // Controleert of alle verplichte gegevens correct zijn ingevuld.
         $request->validate([
             'nieuw_artikelnummer' => 'required',
             'nieuwe_omschrijving' => 'required',
@@ -43,14 +48,14 @@ class WijzigingsverzoekController extends Controller
             'status'              => 'wachtend',
         ]);
 
-        // Stuur melding naar admin
+        // Maakt een melding aan zodat de administrator het verzoek kan beoordelen.
         $materiaal = Materiaal::find($id);
         Melding::create([
             'titel'   => 'Wijzigingsverzoek van magazijnier',
             'bericht' => 'Magazijnier wil artikel ' . $materiaal->artikelnummer . ' wijzigen.',
             'gelezen' => false,
         ]);
-
+    // Stuurt de gebruiker terug naar het materiaaloverzicht.
         return redirect('/materiaal')->with('succes', 'Wijzigingsverzoek ingediend! Wacht op goedkeuring van de admin.');
     }
 }
